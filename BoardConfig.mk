cee5395be046ceae830f84a51f6a1e9b1438238d
Peng Xu <pxu@qca.qualcomm.com>
2017-02-07 15:16:52 -0800

    lisa: Enable support for IEEE80211AX
     * This is to allow supporting WiFi 6 mobile hotspots,
       to be noted this is only supported on SDM855+ platforms.
    
    Change-Id: Iab61bd12499968e8823b3078acd1c7b8bce46136
    [jjpprrrr: skipping CONFIG_IEEE80211AC because external/
    wpa_supplicant_8/wpa_supplicant/defconfig has it enabled by default]
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -245,6 +245,7 @@ WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 CONFIG_ACS := true
+CONFIG_IEEE80211AX := true
 
 # Inherit proprietary blobs
 -include vendor/xiaomi/lisa/BoardConfigVendor.mk
