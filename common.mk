8cd50b7bf21035141c6eecd7a13ca5ae91a037b2
Vaibhav Agrawal <vagrawa@codeaurora.org>
2020-11-19 15:00:03 +0530

    sm8350-common: shima: Support ufs and emmc variant fstab files
    Two fstabs are needed to support different data unit number (dun)
    size for ufs and emmc hardwares. This needs different encryption
    flag in fstab to address the difference in dun size.
    
    [ArianK16a: don't add the emmc fstab since all our devices are ufs]
    
    Test: Verified bootup, vts_kernel_encryption, fscryptctl.
    
    Change-Id: Iff03422d39c789c544bdd86faef070ae46eb4e0e

--- common.mk
+++ common.mk
@@ -180,9 +180,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/charger_fstab.qcom:$(TARGET_COPY_OUT_VENDOR)/etc/charger_fstab.qcom
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.qcom \
-    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.qcom \
-    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.default \
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.default \
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.default
 
 # GPS
 PRODUCT_PACKAGES += \
