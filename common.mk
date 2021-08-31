895eb79e11448dd3c372283c12893dd22c59c101
Cosmin Tanislav <demonsingur@gmail.com>
2021-09-01 01:54:48 +0300

    DNM: sm8350-common: do not build power service

--- common.mk
+++ common.mk
@@ -326,8 +326,8 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Power
-PRODUCT_PACKAGES += \
-    android.hardware.power@1.3-service.xiaomi_sm8350
+# PRODUCT_PACKAGES += \
+#     android.hardware.power@1.3-service.xiaomi_sm8350
 
 # RCS
 PRODUCT_PACKAGES += \
