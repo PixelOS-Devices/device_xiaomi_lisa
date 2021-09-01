f312f2c16f152bc75df9973e63088c919f8d14c0
Cosmin Tanislav <demonsingur@gmail.com>
2021-09-01 10:51:11 +0300

    sm8350-common: remove graphics composer service

--- common.mk
+++ common.mk
@@ -171,8 +171,6 @@ PRODUCT_PACKAGES += \
     memtrack.lahaina
 
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.composer@2.4-impl \
-    android.hardware.graphics.composer@2.4-service \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     android.hardware.memtrack@1.0-impl \
