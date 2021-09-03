c28a95caee052582c342110214db4808e919a864
Cosmin Tanislav <demonsingur@gmail.com>
2021-09-03 20:23:13 +0300

    sm8350-common: update recovery configuration

--- common.mk
+++ common.mk
@@ -288,8 +288,7 @@ PRODUCT_PACKAGES += \
 
 # Recovery
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/init.recovery.qcom.rc:recovery/root/init.recovery.qcom.rc \
-    $(LOCAL_PATH)/rootdir/bin/init.recovery.qcom.sh:recovery/root/init.recovery.qcom.sh
+    $(LOCAL_PATH)/rootdir/etc/init.recovery.qcom.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.qcom.rc
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \
--- rootdir/bin/init.recovery.qcom.sh
+++
@@ -1,9 +0,0 @@
-#!/system/bin/sh
-
-while [ ! -d /dev/block/mapper ]; do
-    sleep 1
-done
-
-ln -s /dev/block/mapper/* /dev/block/bootdevice/by-name/
-
-exit 0
