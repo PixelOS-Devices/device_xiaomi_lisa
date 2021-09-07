d98f1cb8f6044cc08bb7820361aa2916622c87ae
Cosmin Tanislav <demonsingur@gmail.com>
2021-09-07 11:55:45 +0300

    sm8350-common: remove product prop list

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -222,7 +222,6 @@ TARGET_BOARD_PLATFORM := lahaina
 
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
-TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 
