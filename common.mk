9ae9bdc57efff168d3a7a82b228c329c120eab16
Cosmin Tanislav <demonsingur@gmail.com>
2021-09-17 17:08:45 +0300

    sm8350-common: build qti power hal

--- common.mk
+++ common.mk
@@ -351,6 +351,13 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power-service-qti
+
+PRODUCT_COPY_FILES += \
+        $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+
 # RCS
 PRODUCT_PACKAGES += \
     com.android.ims.rcsmanager \
