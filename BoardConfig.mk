f9c1b54e35ecf585471bd7df6a5bb0eea91d2323
Chenyang Zhong <zhongcy95@gmail.com>
2021-10-09 11:30:34 -0400

    lisa: Use SYSTEM_EXT_{PUBLIC,PRIVATE}_SEPOLICY_DIRS
    BOARD_PLAT_PRIVATE_SEPOLICY_DIR and BOARD_PLAT_PUBLIC_SEPOLICY_DIR
    has been deprecated.
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -200,8 +200,8 @@ VENDOR_SECURITY_PATCH := 2022-01-01
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Soong
