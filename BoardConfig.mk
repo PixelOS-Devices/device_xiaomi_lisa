484280d764a5a8a4441a0e3d2bfaa34659d37b16
Arian <arian.kulmer@web.de>
2021-10-13 23:50:11 +0200

    lisa: Set dtbo and userdata partition sizes
    Change-Id: I9e61f6abcbefba66af92b3967df80fc74b084753

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,5 +13,9 @@ include device/xiaomi/sm8350-common/BoardConfigCommon.mk
 # Board
 TARGET_BOOTLOADER_BOARD_NAME := lisa
 
+# Partitions
+BOARD_DTBOIMG_PARTITION_SIZE := 25165824
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 113254576128
+
 # Include proprietary files
 include vendor/xiaomi/lisa/BoardConfigVendor.mk
