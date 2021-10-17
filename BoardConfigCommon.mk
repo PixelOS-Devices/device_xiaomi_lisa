bf652618b878dfac5962264d40d91baa90cab321
Cosmin Tanislav <demonsingur@gmail.com>
2021-10-17 20:49:09 +0300

    sm8350-common: switch to enforcing selinux

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -118,7 +118,6 @@ BOARD_KERNEL_CMDLINE += pcie_ports=compat
 BOARD_KERNEL_CMDLINE += iptable_raw.raw_before_defrag=1
 BOARD_KERNEL_CMDLINE += ip6table_raw.raw_before_defrag=1
 BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 # Kernel modules
 BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(COMMON_PATH)/modules.load))
