a3f44f5fec9243c9d24a60141730f104cd5c7656
bibarub <bibarub@zohomail.eu>
2021-10-20 21:42:58 +0300

    lisa: Build android.hardware.power@1.2.vendor

--- device.mk
+++ device.mk
@@ -414,6 +414,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
+    android.hardware.power@1.2.vendor \
     vendor.qti.hardware.perf@2.2.vendor
 
 PRODUCT_COPY_FILES += \
