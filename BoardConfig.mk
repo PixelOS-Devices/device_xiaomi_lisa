575c2606cfa3f360e75501a7886fa36a01c219b6
Arian <arian.kulmer@web.de>
2021-11-07 19:17:23 +0100

    lisa: Set bootloader board name
    Change-Id: Ied8de2941ccc455c7755f50e3843eadde59f1ad8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,5 +10,8 @@ DEVICE_PATH := device/xiaomi/lisa
 # Inherit from sm8350-common
 include device/xiaomi/sm8350-common/BoardConfigCommon.mk
 
+# Board
+TARGET_BOOTLOADER_BOARD_NAME := lisa
+
 # Include proprietary files
 include vendor/xiaomi/lisa/BoardConfigVendor.mk
