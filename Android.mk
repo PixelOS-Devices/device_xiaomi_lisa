9a07f78730de1dc2bc4afdc269b1188065c56242
Nico Kümmel <nico.kuemmel@gmail.com>
2021-11-16 13:26:21 +0100

    sm8350-common: Add support for odin
    Change-Id: I7a657b9a8c5732860bb120472f14fe4c83fb6e22

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter haydn lisa venus,$(TARGET_DEVICE)),)
+ifneq ($(filter haydn lisa odin venus,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
