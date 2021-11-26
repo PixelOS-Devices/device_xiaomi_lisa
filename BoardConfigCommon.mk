78c5b482916a66790e8294e8981285e6a09c382d
Cosmin Tanislav <demonsingur@gmail.com>
2021-11-26 03:29:46 +0200

    sm8350-common: manifests: remove prebuilt health hal manifest

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -130,7 +130,6 @@ DEVICE_MANIFEST_FILE := \
     $(COMMON_PATH)/manifests/AHBF@2.1-service.xml \
     $(COMMON_PATH)/manifests/android.hardware.atrace@1.0-service.xml \
     $(COMMON_PATH)/manifests/android.hardware.gnss@2.1-service-qti.xml \
-    $(COMMON_PATH)/manifests/android.hardware.health@2.1.xml \
     $(COMMON_PATH)/manifests/android.hardware.ir@1.0-service.xml \
     $(COMMON_PATH)/manifests/android.hardware.neuralnetworks@1.3-service-qti.xml \
     $(COMMON_PATH)/manifests/c2_manifest_vendor.xml \
