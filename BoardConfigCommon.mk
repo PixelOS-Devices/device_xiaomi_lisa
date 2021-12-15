941f73234ac322c8c0c1245e98a1929149892499
Erfan Abdi <erfangplus@gmail.com>
2021-12-15 16:44:56 +0330

    sm8350-common: Drop vendor modules load
    Change-Id: Ibd9c586c62cbb28b145bf98c6fb5bef9a5b2ae8b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -112,10 +112,6 @@ BOARD_KERNEL_CMDLINE += iptable_raw.raw_before_defrag=1
 BOARD_KERNEL_CMDLINE += ip6table_raw.raw_before_defrag=1
 BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
 
-# Kernel modules
-BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(COMMON_PATH)/modules.load))
-BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(COMMON_PATH)/modules.load.recovery))
-
 # HIDL
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(COMMON_PATH)/framework_manifest.xml
