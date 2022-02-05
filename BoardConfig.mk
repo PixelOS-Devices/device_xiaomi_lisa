5dfb23e254e7069705fc6386af495dbbf54c4009
ItsVixano <giovanniricca@protonmail.com>
2022-02-05 08:49:33 +0100

    lisa: Setup kernel flags
    Change-Id: I2f8017bec64149fbd006d31c33fb77c1c0d9b89f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,6 +13,17 @@ include device/xiaomi/sm8350-common/BoardConfigCommon.mk
 # Board
 TARGET_BOOTLOADER_BOARD_NAME := lisa
 
+# Kernel
+TARGET_KERNEL_CONFIG := lisa_defconfig
+
+# Kernel modules
+BOOT_KERNEL_MODULES := \
+    hwid.ko \
+    goodix_core.ko \
+    xiaomi_touch.ko
+
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(BOOT_KERNEL_MODULES)
+
 # Partitions
 BOARD_DTBOIMG_PARTITION_SIZE := 25165824
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 113254576128
