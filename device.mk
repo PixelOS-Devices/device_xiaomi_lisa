64d200cd13f0ab5b74b177611dd4359878740511
Sonal Singh <sonal.singh.19993@gmail.com>
2022-02-10 23:10:20 +0300

    lisa: Build libminijail from source

--- device.mk
+++ device.mk
@@ -359,6 +359,7 @@ PRODUCT_COPY_FILES += \
 
 # Minijail
 PRODUCT_PACKAGES += \
+    libminijail \
     libavservices_minijail \
     libavservices_minijail.vendor \
     libcodec2_hidl@1.1.vendor \
