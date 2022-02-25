0625d4f18a6faa5d3062499a68f9594143ac3ec8
Arian <arian.kulmer@web.de>
2022-02-25 20:15:48 +0100

    Revert "sm6250-common: copy libhidlcache from vndk 29"
    This reverts commit 9abdfa8c4b7fc267ef7fef04cc3858657a733bf6.
    
    Change-Id: Idaf145ab0dffe3db00cf7c91926843c664f5f2cc

--- common.mk
+++ common.mk
@@ -439,9 +439,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # VNDK
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libhidlcache.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlcache.so
-
 # vndservicemanager has been removed from API30 devices (aosp/1235751)
 # but we still need it for display services.
 PRODUCT_PACKAGES += \
