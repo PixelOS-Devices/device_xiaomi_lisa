1f07b44542c2272787228631f77941b2324d823f
Arian <arian.kulmer@web.de>
2022-02-26 13:10:03 +0100

    sm8350-common: Set ro.media.xml_variant.profiles to choose media profiles
    This was introduced with [1] and allows to choose target specific media
    profiles without caf changes to frameworks/av. Unset media.settings.xml
    so that the new property gets respected and drop copying the lahaina
    config as media_profiles_vendor since we don't need a fallback anymore.
    
    [1]: https://github.com/LineageOS/android_frameworks_av/commit/eaac82f33f2a064c6ae1a5f1791bb13669820687#
    
    Change-Id: I7ec306bf3dd595c09308ed26c9d3a6db8490c7f0

--- common.mk
+++ common.mk
@@ -268,7 +268,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/init.qti.media.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.qti.media.rc
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/media/lahaina/media_profiles_lahaina.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_vendor.xml \
     $(LOCAL_PATH)/media/media_codecs_vendor_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor_audio.xml
 
 PRODUCT_COPY_FILES += \
