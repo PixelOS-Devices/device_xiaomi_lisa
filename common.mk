29e5b0c18689387ca7832fcb42d391c01146182c
Arian <arian.kulmer@web.de>
2022-02-28 16:05:41 +0100

    sm8350-common: Import qti_whitelist
    Change-Id: Ic7dc5fecf3d2fc041acdb3c3472f519f3ad573da

--- common.mk
+++ common.mk
@@ -362,6 +362,7 @@ PRODUCT_PACKAGES += \
     libqti_vndfwk_detect.vendor
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
     $(LOCAL_PATH)/configs/telephony_system-ext_privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/telephony_system-ext-privapp-permissions-qti.xml
 
 # Recovery
