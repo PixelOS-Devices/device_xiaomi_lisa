76ba94d989175fab3b22b7fa2ac7eae2b061693d
Arian <arian.kulmer@web.de>
2022-02-28 14:52:25 +0100

    sm8350-common: Build qti camera postproc interface to vendor
    Change-Id: I7c90727e85455323c8f9fb2229fe414ba74379e1

--- common.mk
+++ common.mk
@@ -133,7 +133,8 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64
+    android.hardware.camera.provider@2.4-service_64 \
+    vendor.qti.hardware.camera.postproc@1.0.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
