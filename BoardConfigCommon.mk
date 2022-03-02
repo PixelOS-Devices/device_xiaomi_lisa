2eb8ab7ef0fbc46ce038bbedbd1242ee1f966d1d
Arian <arian.kulmer@web.de>
2022-03-02 18:53:49 +0100

    sm8350-common: Checkout framework compatibility matrix to LA.QSSI.11.0.r1-12100-qssi.0
    Additonally move xiaomi's additions to xiaomi_framework_compatibility_matrix.xml
    and make them optional.
    
    Change-Id: Ib3abddb4a9e9615fb973b9efd2e511d96998d6ce

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -99,7 +99,8 @@ BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/hidl/compatibility_matrix.xml
 
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    $(COMMON_PATH)/hidl/framework_compatibility_matrix.xml \
+    $(COMMON_PATH)/hidl/vendor_framework_compatibility_matrix.xml \
+    $(COMMON_PATH)/hidl/xiaomi_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 
 DEVICE_MANIFEST_FILE := \
