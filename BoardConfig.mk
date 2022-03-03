648764843d46295da945dd607febbe651ad1c08d
Arian <arian.kulmer@web.de>
2022-03-03 01:01:35 +0100

    lisa: Define ramdisk kernel modules
    Change-Id: I90e8a7206e0493cf9e8a93d4786daf0e9927c539

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -118,6 +118,14 @@ BOARD_KERNEL_CMDLINE += iptable_raw.raw_before_defrag=1
 BOARD_KERNEL_CMDLINE += ip6table_raw.raw_before_defrag=1
 BOARD_KERNEL_CMDLINE += video=vfb:640x400,bpp=32,memsize=3072000
 
+# Kernel modules
+BOOT_KERNEL_MODULES := \
+    hwid.ko \
+    goodix_core.ko \
+    xiaomi_touch.ko
+
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(BOOT_KERNEL_MODULES)
+
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072
 BOARD_BOOTIMAGE_PARTITION_SIZE := 201326592
