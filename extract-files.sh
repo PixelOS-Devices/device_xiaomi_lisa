12cedd0213d8e26ce642299e254439441be56495
Karan Parashar <whyredfire@gmail.com>
2022-03-23 17:20:08 +0530

    lisa: Import scripts for proprietary extraction
    - from https://github.com/LineageOS/android_tools_extract-utils/tree/lineage-19.1/templates/multi-device/device
    
    Change-Id: I4c4308fb7887fefe6828b6b94f73637dba8aa8b5

---
+++ extract-files.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib/libsample1.so)
+            sed -i 's|/data/misc/sample1|/data/misc/sample2|g' "${2}"
+            ;;
+        vendor/lib64/libsample2.so)
+            "${PATCHELF}" --remove-needed "libsample3.so" "${2}"
+            "${PATCHELF}" --add-needed "libsample4.so" "${2}"
+            ;;
+    esac
+}
+
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
+if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
+    return
+fi
+
+set -e
+
+export DEVICE=lisa
+export DEVICE_COMMON=sm8350-common
+export VENDOR=xiaomi
+
+"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
