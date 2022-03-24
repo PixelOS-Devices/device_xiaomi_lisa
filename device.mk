4bcded9802066edbb081422d86dcd50135851991
althafvly <althafvly@gmail.com>
2022-03-24 17:18:43 +0530

    lisa: Inherit launch_with_vendor_ramdisk for vAB
    - sm8350 devices have a vendor_boot partition
    
    Change-Id: I5c344890bc12ee769e2f27369a4151b469271a51

--- device.mk
+++ device.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Enable virtual A/B OTA
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
