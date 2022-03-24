6abd5ad47e7f5bb327cacf885fb72b5e36d6d0af
althafvly <althafvly@gmail.com>
2022-03-24 17:07:34 +0530

    lisa: Inherit vAB makefile from new dir
    Change-Id: I10d914c833f460b7b1fab6d357e4b1e255098bac

--- device.mk
+++ device.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Enable virtual A/B OTA
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch.mk)
 
 # Enable project quotas and casefolding for emulated storage without sdcardfs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
