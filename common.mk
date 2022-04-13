d0da9ffea8063352e157a5a5fb1c3966f5769fbd
Arian <arian.kulmer@web.de>
2022-04-14 00:12:50 +0200

    sm8350-common: Don't explicitly build fs_config_files
    The fs_config_files for each partition is already added to
    PRODUCT_PACKAGES by base_{product,system,system_ext,vendor}.mk.
    
    Change-Id: Ifbadacd5cb20beca70ed8b3accb3baf86a989c92

--- common.mk
+++ common.mk
@@ -26,10 +26,6 @@ $(call inherit-product-if-exists, vendor/xiaomi/sm8350-common/sm8350-common-vend
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
-# AID/fs configs
-PRODUCT_PACKAGES += \
-    fs_config_files
-
 # API
 PRODUCT_SHIPPING_API_LEVEL := 30
 
