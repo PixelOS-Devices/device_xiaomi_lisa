72fd2db160fb94cb36386dc671c24bd5d7db8711
CannedShroud <hypersync112@gmail.com>
2022-04-25 20:56:35 -0400

    lisa: pass LLVM_IAS=1 for kernel compile
    Change-Id: I5b2639aa593e4b9e93197aedebf257b891275e0a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -101,6 +101,7 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc \
     LLVM=1 \
+    LLVM_IAS=1 \
     TARGET_PRODUCT=$(PRODUCT_DEVICE)
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_SOURCE := kernel/xiaomi/lisa
