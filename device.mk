c191079932226bb31e63aaa16b1e5f6a1f4936d1
Chenyang Zhong <zhongcy95@gmail.com>
2022-04-25 20:56:35 -0400

    lisa: bump soundtrigger impl to 2.3
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Change-Id: Iac47a8895cc7d5805de5ef6b71ea553ac8a96bb2

--- device.mk
+++ device.mk
@@ -46,7 +46,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl:32 \
     android.hardware.audio.effect@7.0-impl:32 \
     android.hardware.audio.service \
-    android.hardware.soundtrigger@2.2-impl
+    android.hardware.soundtrigger@2.3-impl
 
 PRODUCT_PACKAGES += \
     libbatterylistener \
