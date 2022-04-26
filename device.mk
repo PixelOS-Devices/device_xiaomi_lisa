7c99c6557ae44f4e0cdc6ba40a67c1ec0bab4d39
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-04-25 20:56:35 -0400

    lisa: Build android.hidl.base@1.0 - needed for secure_element
    Change-Id: I5457ebbadd95e4c43af4f4dd2e1a1413a241d865
    Signed-off-by: bheatleyyy <sugakesshaa@gmail.com>

--- device.mk
+++ device.mk
@@ -259,6 +259,8 @@ PRODUCT_PACKAGES += \
 
 # Hidl
 PRODUCT_PACKAGES += \
+    android.hidl.base@1.0 \
+    android.hidl.base@1.0.vendor \
     libhidltransport \
     libhidltransport.vendor \
     libhwbinder \
