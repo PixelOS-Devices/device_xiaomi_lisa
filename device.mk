b4af22fb4516cb8961b688c716da39aaaf4b25a9
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-04-25 20:56:35 -0400

    lisa: rootdir: Fixup oem13 sensor entry in /persist
     * Xiaomi for no apparent reason renamed oem13 to
       oem13_light_smd in MIUI 13. This causes sensor HAL
       to read existing oem13 entry in persist sensors_list
       and tries to look for it but since it doesn't exist,
       it delays boot for 15 seconds:
    
    03-20 03:02:14.362   833   833 E sensors-hal: wait_for_mandatory_sensors:503, some mandatory sensors not available even after 15 seconds, giving up.
    03-20 03:02:14.362   833   833 E sensors-hal: wait_for_mandatory_sensors:505, 1 missing sensor(s)
    03-20 03:02:14.362   833   833 E sensors-hal: wait_for_mandatory_sensors:508,     oem13
    
     * Remove the old oem13 entry at init to resolve this
       nonsense by xiaomi. Thanks to Daniel (@ djchittoor)
       for pointing out the fix.
    
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: Iaded6f9240e4f4d2cd81f86e9b9be41e7ccf1c2c

--- device.mk
+++ device.mk
@@ -271,7 +271,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/init.qcom.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.qcom.rc \
     $(LOCAL_PATH)/rootdir/etc/init.qcom.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.qcom.usb.rc \
     $(LOCAL_PATH)/rootdir/etc/init.qti.kernel.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.qti.kernel.rc \
-    $(LOCAL_PATH)/rootdir/etc/init.target.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.target.rc
+    $(LOCAL_PATH)/rootdir/etc/init.target.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.target.rc \
+    $(LOCAL_PATH)/rootdir/bin/init.sensors_fix.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.sensors_fix.sh
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/bin/init.kernel.post_boot-lahaina.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.kernel.post_boot-lahaina.sh \
