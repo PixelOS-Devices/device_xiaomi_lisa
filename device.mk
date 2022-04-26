e3a1387c05eef209969e994400e8971e3593bf07
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-04-25 20:56:35 -0400

    lisa: Uprev Bluetooth audio HIDL impl to 2.1
    Change-Id: I63211c9877c19d089e7cafa5a3cc52a888309dca

--- device.mk
+++ device.mk
@@ -112,7 +112,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth@1.1.vendor \
-    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio@2.1-impl \
     com.dsi.ant@1.0.vendor \
     com.qualcomm.qti.bluetooth_audio@1.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
