a741372c7d61de97d0e4c4457699da090a721d7e
Lucchetto <lucchetto.tie@live.com>
2022-04-25 20:56:35 -0400

    lisa: create dummy libqti-perfd-client
    * This way, proprietary perfd blobs can be removed without breaking
      fingerprint
    
    Change-Id: I124652f3041761966a3e3bd97c757fecc39cc5fb

--- device.mk
+++ device.mk
@@ -413,7 +413,8 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
     vendor.qti.hardware.perf@2.2.vendor \
-    android.hardware.power-service.lisa-libperfmgr
+    android.hardware.power-service.lisa-libperfmgr \
+    libqti-perfd-client
     
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf \
