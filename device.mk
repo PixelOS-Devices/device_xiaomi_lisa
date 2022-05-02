795aee8c12c5995c6c3689660b7dbecc8808a7c3
Bruno Martins <bgcngm@gmail.com>
2022-05-02 14:06:56 +0100

    lisa: Switch to source-built mlipay interface
    Change-Id: I7daaaffd9fe7a870e42c1d882b52dc93b3231120

--- device.mk
+++ device.mk
@@ -349,6 +349,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_video.xml
 
+# Mlipay
+PRODUCT_PACKAGES += \
+    vendor.xiaomi.hardware.mlipay@1.1.vendor
+
 # Minijail
 PRODUCT_PACKAGES += \
     libminijail \
