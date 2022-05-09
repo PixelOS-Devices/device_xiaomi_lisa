6fc5b5f62f8853663680bf4f11718c01a202e1d1
althafvly <althafvly@gmail.com>
2022-05-09 18:26:59 +0530

    lisa: Use randomly generated camera config
    Change-Id: Ib3f6f1526e2e0b7db23bb34c5b15aec1b0d338c2

--- device.mk
+++ device.mk
@@ -155,6 +155,9 @@ PRODUCT_VENDOR_PROPERTIES += \
     ro.hardware.camera=xiaomi
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/camera_cnf.txt:$(TARGET_COPY_OUT_VENDOR)/etc/camera/camera_cnf.txt
+
+PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
