d9cf31af62c4f434782f3c8a3ef00a4072f504ad
bheatleyyy <sugakesshaa@gmail.com>
2022-05-09 16:00:05 +0800

    Revert "lisa: create dummy libqti-perfd-client"
    This reverts commit a741372c7d61de97d0e4c4457699da090a721d7e.

--- device.mk
+++ device.mk
@@ -416,8 +416,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
     vendor.qti.hardware.perf@2.2.vendor \
-    android.hardware.power-service.lisa-libperfmgr \
-    libqti-perfd-client
+    android.hardware.power-service.lisa-libperfmgr
     
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf \
