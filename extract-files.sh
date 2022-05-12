b6eddcb163fff00b5ea9a6e9b7ad6dffc94a7fed
EcrosoftXiao <xy19990903@gmail.com>
2022-05-12 11:44:35 +0800

    !fixup: Add support for Dolby Vision - subsequent to f06f962

--- extract-files.sh
+++ extract-files.sh
@@ -61,9 +61,8 @@ function blob_fixup() {
         vendor/etc/camera/pure*_parameter.xml)
             sed -i 's/=\([0-9]\+\)>/="\1">/g' "${2}"
             ;;
-        odm/etc/vintf/manifest/c2_manifest_xiaomi.xml)
-            sed -i 's|<hal format="hidl">|<hal format="hidl" override="true">|g' "${2}"
-            sed -i "/ozoaudio/d" "${2}"
+        vendor/etc/vintf/manifest/c2_manifest_vendor.xml)
+            sed -ni '/ozoaudio/!p' "${2}"
             ;;
         vendor/lib64/hw/camera.xiaomi.so)
             # Before
