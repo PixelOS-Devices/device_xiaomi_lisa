5f2e54ed67f09855d4a6c166f93a7452418f0aa3
Alexander Koskovich <akoskovich@pm.me>
2022-05-15 02:56:04 +0530

    lisa: Drop all component overrides
    * Qualcomm no longer disables these for targets
      that support RRO, e.g. all QSSI targets.
    
    Change-Id: I0372817623e8d0e58926fbfb519671c3caa07b86

--- device.mk
+++ device.mk
@@ -72,11 +72,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
-# Component override
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml \
-    $(LOCAL_PATH)/configs/component-overrides_qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/component-overrides.xml
-
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
