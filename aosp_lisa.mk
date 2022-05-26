da5b95129cd1312c5946138648370520f65e08ec
Karan Parashar <whyredfire@gmail.com>
2022-05-26 11:46:05 +0530

    lisa: Define bootanimation resolution
    Change-Id: I2ce677042683a9affd586dbd605f36ada8c70d47

--- aosp_lisa.mk
+++ aosp_lisa.mk
@@ -13,6 +13,7 @@ $(call inherit-product, device/xiaomi/lisa/device.mk)
 
 # Inherit some common PixelOS stuff
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_SUPPORTS_QUICK_TAP := true
 
