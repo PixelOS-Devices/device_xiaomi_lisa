c6d211fb6bc979caabdd5081ac06e34b327fe37e
Bowgo Tsai <bowgotsai@google.com>
2022-06-13 02:57:19 +0000

    lisa: Removing GSI keys
    Those keys will be embedded into VtsSecurityAvb on host side
    instead, to verify the GSI image used on the device.
    
    Bug: 149806769
    Test: build and checks those keys are removed from
          $OUT/recovery/root/first_stage_ramdisk/avb/
    Change-Id: Id0166d1c4dd07067bff49c28ca3469b5ca74d7cf

--- device.mk
+++ device.mk
@@ -8,9 +8,6 @@
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
-# Installs gsi keys into ramdisk, to boot a GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
 # Enable virtual A/B OTA
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 
