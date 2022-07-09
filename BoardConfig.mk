e209335aca603999a2cc26d7d93f48111d1a93ed
Karan Parashar <whyredfire@gmail.com>
2022-07-09 13:49:23 +0530

    lisa: Bump vendor security patch level
    https://dumps.tadiphone.dev/dumps/qti/missi/-/blob/missi-user-12-SKQ1.211006.001-V13.0.4.0.SKOMIXM-release-keys/vendor/build.prop#L4
    
    Change-Id: I41e6ae0bd40e37e05e00883df0a2a8dab666c580

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -211,7 +211,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2022-01-01
+VENDOR_SECURITY_PATCH := 2022-04-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
