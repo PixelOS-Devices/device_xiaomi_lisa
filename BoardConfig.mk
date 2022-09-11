9d025bda33a8bc98b88d7d8a80fcb6cd369e5767
Giovanni Ricca <giovanniricca@proton.me>
2022-09-11 22:08:03 +0200

    lisa: Include device specific kernel config fragment
    Change-Id: Id5cacc4f1caf8edb3defc1f793a0c501039269f1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,5 +9,8 @@ DEVICE_PATH := device/xiaomi/lisa
 # Inherit from sm8350-common
 include device/xiaomi/sm8350-common/BoardConfigCommon.mk
 
+# Kernel
+TARGET_KERNEL_CONFIG += vendor/lisa_QGKI.config
+
 # Include proprietary files
 include vendor/xiaomi/lisa/BoardConfigVendor.mk
