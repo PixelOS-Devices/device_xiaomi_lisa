fd5eb27687af77905fb2fef4ade7a478eae4e506
LuK1337 <priv.luk@gmail.com>
2022-10-31 21:59:52 +0100

    lisa: overlay: Configure aux camera for Aperture
    Change-Id: I543f078dd4dba30e1b22737d5c22d591e3fb34e6

--- device.mk
+++ device.mk
@@ -400,7 +400,8 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
     AOSPLisaCSOTFrameworksOverlay
