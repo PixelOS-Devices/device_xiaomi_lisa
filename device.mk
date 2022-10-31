f60fb5c97699c5583690b397f59f98b4732620d8
LuK1337 <priv.luk@gmail.com>
2022-10-31 21:59:52 +0100

    lisa: overlay-lineage: Configure aux camera for Aperture
    Change-Id: I543f078dd4dba30e1b22737d5c22d591e3fb34e6

--- device.mk
+++ device.mk
@@ -9,7 +9,8 @@ $(call inherit-product, device/xiaomi/sm8350-common/common.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # Overlays-RRO
 PRODUCT_PACKAGES += \
