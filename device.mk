17ca5203c930fdcbe80958542e84b57606fe2869
Karan Parashar <whyredfire@gmail.com>
2022-11-05 00:05:43 +0530

    lisa: Migrate color lib config to display configs
    Change-Id: I73ea9013a5885d16c355b19e3d870466b9dc9832

--- device.mk
+++ device.mk
@@ -191,7 +191,7 @@ PRODUCT_PACKAGES += \
     lights.lisa
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/snapdragon_color_libs_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/snapdragon_color_libs_config.xml \
+    $(LOCAL_PATH)/configs/display/snapdragon_color_libs_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/snapdragon_color_libs_config.xml \
     $(LOCAL_PATH)/configs/display/display_id_4630946480857061762.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_4630946480857061762.xml
 
 # DRM
