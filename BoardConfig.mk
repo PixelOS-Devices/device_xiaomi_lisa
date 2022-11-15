bf96b5d58c523d7cc6681199b00baa25d2ccf3a9
Karan Parashar <karan@pixelos.net>
2022-11-15 20:34:27 +0000

    lisa: Bump vendor security patch level
    Change-Id: Id0a4b3f2ef1db0b1ac6125e8c07af7d1c9a6da62

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -206,7 +206,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2022-04-01
+VENDOR_SECURITY_PATCH := 2022-10-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
