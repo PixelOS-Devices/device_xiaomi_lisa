71944933df7c59130f01a36bba08c126ea2a940a
Karan Parashar <karan@pixelos.net>
2023-01-01 13:00:49 +0530

    Revert "lisa: gpt_utils: fsync after block device writes"
    This reverts commit 5745727d1fdbf47576aac5701a2fd2ea9708496b.

--- gpt-utils/gpt-utils.cpp
+++ gpt-utils/gpt-utils.cpp
@@ -157,18 +157,11 @@ static int blk_rw(int fd, int rw, int64_t offset, uint8_t *buf, unsigned len)
     else
         r = read(fd, buf, len);
 
-    if (r < 0) {
+    if (r < 0)
         fprintf(stderr, "block dev %s failed: %s\n", rw ? "write" : "read",
                 strerror(errno));
-    } else {
-        if (rw) {
-            r = fsync(fd);
-            if (r < 0)
-                fprintf(stderr, "fsync failed: %s\n", strerror(errno));
-        } else {
-            r = 0;
-        }
-    }
+    else
+        r = 0;
 
     return r;
 }
