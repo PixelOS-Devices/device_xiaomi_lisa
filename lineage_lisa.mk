6d679bc1b56c22a9d9ccf07489bf4f685695f937
Giovanni Ricca <giovanniricca@proton.me>
2023-01-22 17:58:00 +0100

    lisa: Update blobs to V14.0.2.0.TKOMIXM
    Change-Id: I958158403d3adec8fb64a91cbdab05c7e787a1a9

--- lineage_lisa.mk
+++ lineage_lisa.mk
@@ -26,9 +26,9 @@ PRODUCT_SYSTEM_NAME := lisa_global
 PRODUCT_SYSTEM_DEVICE := lisa
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="lisa_global-user 12 SKQ1.211006.001 V13.0.8.0.SKOMIXM release-keys" \
+    PRIVATE_BUILD_DESC="lisa_global-user 13 TKQ1.220829.002 V14.0.2.0.TKOMIXM release-keys" \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := Xiaomi/lisa_global/lisa:12/SKQ1.211006.001/V13.0.8.0.SKOMIXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/lisa_global/lisa:13/TKQ1.220829.002/V14.0.2.0.TKOMIXM:user/release-keys
