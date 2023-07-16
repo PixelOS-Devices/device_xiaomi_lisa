b8f83fa399a8ceb953c0ae7e6abbb277d972cce3
Karan Parashar <karan@pixelos.net>
2023-07-17 00:06:24 +0200

    lisa: Update to miui_LISAGlobal_V14.0.4.0.TKOMIXM
    Change-Id: I9fd323590212cfc38f1259b77147fef58f602d6f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -197,7 +197,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2023-04-01
+VENDOR_SECURITY_PATCH := 2023-06-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
