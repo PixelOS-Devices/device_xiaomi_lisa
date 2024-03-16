839f031a0cb3420860ce53a56ca17153369f1e1e
Edwin Moquete <edwinmmoquete@gmail.com>
2024-03-16 01:24:14 +0000

    lisa: Build android.media.audio.common.types-V2-cpp
    Needed for WFD, and not built default on QPR2
    
    Change-Id: If860613ef4cb6bae7f7a7e72707c24510e36abd6

--- device.mk
+++ device.mk
@@ -548,9 +548,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.rtt.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.rtt.xml
 
 # WiFi Display
-PRODUCT_BOOT_JARS += \
-    WfdCommon
-
 PRODUCT_PACKAGES += \
+    android.media.audio.common.types-V2-cpp \
     libnl \
     libwfdaac_vendor
+
+PRODUCT_BOOT_JARS += \
+    WfdCommon
