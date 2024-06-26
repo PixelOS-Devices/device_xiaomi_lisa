1fec9e5c7c3824cfed677f48e1cee730a0a387c8
Karan Parashar <karan@pixelos.net>
2024-06-27 02:01:58 +0530

    lisa: Drop non-existent packages

--- device.mk
+++ device.mk
@@ -171,8 +171,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     libdisplayconfig.qti \
-    libdisplayconfig.qti.vendor \
-    libdisplayconfig.system \
     libdisplayconfig.system.qti \
     libqdMetaData \
     libqdMetaData.system \
