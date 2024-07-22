d2e6da69a63a0dda1534c7efdf451cc598c19053
Karan Parashar <karan@pixelos.net>
2024-07-22 23:11:24 +0530

    lisa: Adapt for PixelOS
    Change-Id: I667e6cc89c842af91237669f4256d02ef544ce37

--- device.mk
+++ device.mk
@@ -9,8 +9,7 @@ $(call inherit-product, device/xiaomi/sm8350-common/common.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # Overlays-RRO
 PRODUCT_PACKAGES += \
