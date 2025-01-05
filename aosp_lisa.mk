09fbb56cff29da30d954a54fb7143d85a82085d2
Giovanni Ricca <giovanniricca@proton.me>
2025-01-05 09:20:57 +0100

    lisa: Drop `core_64_bit` & `full_base_telephony` inclusions
    * It's already done on sm8350-common
    
    Change-Id: I20d7b8a7db38e7abe583b0dc13db1be80d453de8

--- aosp_lisa.mk
+++ aosp_lisa.mk
@@ -4,10 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
 # Inherit from lisa device
 $(call inherit-product, device/xiaomi/lisa/device.mk)
 
